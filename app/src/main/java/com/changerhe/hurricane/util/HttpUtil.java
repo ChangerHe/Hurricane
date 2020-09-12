b7272797ea47a0e1340b212b3bee9f9da4847893
ChangerHe <244737162@qq.com>
2020-09-12 23:20:48 +0800

    feat: add request api

---
+++ app/src/main/java/com/changerhe/hurricane/util/HttpUtil.java
@@ -0,0 +1,13 @@
+package com.changerhe.hurricane.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient httpClient = new OkHttpClient();
+
+        Request request = new Request.Builder().url(address).build();
+        httpClient.newCall(request).enqueue(callback);
+    }
+}
